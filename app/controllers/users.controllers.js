6268026b52f420cc942906f30f2180c5ef267b25
Sean Madondo <sma297@uclive.ac.nz>
2020-03-18 11:53:24 +1300

    Updates to User - installed Rand - token via NPM

--- app/controllers/users.controllers.js
+++ app/controllers/users.controllers.js
@@ -1,8 +1,34 @@
 const user = require('../models/users.model');
 
+
+//Check email address is valid
+function checkEmailValidity(email) {
+    return email.includes('@')
+}
+
 exports.register = async function(req, res) {
+    console.log('\nRequest to register a new user....');
 
+    if (!checkEmailValidity(req.body.email)) {
+        console.log("Bad email address entered...");
+        res.status(500)
+            .send(`${email} IS NOT A VALID EMAIL ADDRESS`);
+    } else {
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        const city = req.body.city;
+        const country = req.body.country;
 
+        try {
+            const result = await user.register(name, email, password, city, country);
+            res.status(200)
+                .send('User Successfully Registered!');
+        } catch (err) {
+            res.status(500)
+                .send(`ERROR Registering User ${name}: ${err}`);
+        }
+    }
 };
 
 exports.login = async function(req, res) {
@@ -16,8 +42,21 @@ exports.logout = async function(req, res) {
 };
 
 exports.retrieve = async function(req, res) {
-
-
+    console.log("\nRequest to retrieve a user....");
+    const id = req.params.id;
+    try {
+        const result = await user.retrieve(id);
+        if (result.length === 0) {
+            res.status(400)
+                .send('Invalid Id');
+        } else {
+            res.status(200)
+                .send(result);
+        }
+    } catch (err) {
+        res.status(500)
+            .send(`ERROR retrieving user ${id}: ${err}`);
+    }
 };
 
 exports.change = async function(req, res) {
--- app/models/users.model.js
+++ app/models/users.model.js
@@ -1,17 +1,24 @@
 const db = require('../../config/db');
 const fs = require('mz/fs');
+const randomtoken = require('rand-token');
 
 const photoDirectory = './storage/photos/';
 const defaultPhotoDirectory = './storage/default/';
 
-exports.register = async function () {
-
-
+exports.register = async function (c_name, c_email, c_password, c_city,c_country) {
+    console.log('Request to register a user to the database.....');
+    const conn = await db.getPool().getConnection();
+    const query = 'insert into users (name, email, password, city, country) values (?, ?, ?, ?, ?)';
+    const [result] = await conn.query(query, [c_name, c_email, c_password, c_city=NULL, c_country=NULL]);
+    conn.release();
+    return result;
 };
 
-exports.login = async function () {
-
-
+exports.login = async function (userID) {
+    console.log('Request to login User....');
+    const loginSQL = 'UPDATE User SET auth_token = ? WHERE user_id = ?';
+    const token = randomtoken.generate(32);
+    
 };
 
 
@@ -21,9 +28,13 @@ exports.logout = async function () {
 
 };
 
-exports.retrieve = async function () {
-
-
+exports.retrieve = async function (id) {
+    console.log(`Request to retrieve user ${id} from the database...`);
+    const conn = await db.getPool().getConnection();
+    const query = 'select * from User where user_id = ?';
+    const [rows] = await conn.query(query, [id]);
+    conn.release();
+    return rows;
 };
 
 exports.change = async function () {
